1b66596a68781090a41da540e627eff4dddbc45c
Ryan Powszok <ryan@jukelabs.com>
2020-06-28 23:18:00 -0500

    Updates

---
+++ precache-manifest.8486f74f858beaf5ae6d2c84ff65229e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9c66e74bbd1471b29da8851888d37a",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "224c11275d6b1e29dcf8",
+    "url": "/memory-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef394be3f657df863870",
+    "url": "/memory-game/static/js/2.b656477a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game/static/js/2.b656477a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224c11275d6b1e29dcf8",
+    "url": "/memory-game/static/js/main.201ddab5.chunk.js"
+  },
+  {
+    "revision": "a7295d09a3c7d39f3388",
+    "url": "/memory-game/static/js/runtime-main.cd8959cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memory-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
